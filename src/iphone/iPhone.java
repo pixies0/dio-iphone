44dfc24c7344804b3c6d5d1ef656356c15ff1557
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-07-23 07:25:31 -0300

    :zap: Classe concreta implementando seus métodos

---
+++ src/iphone/iPhone.java
@@ -0,0 +1,81 @@
+package iphone;
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String modelo;
+    private String versaoSO;
+
+    public iPhone() {
+        this.modelo = "iPhone Genérico";
+        this.versaoSO = "iOS 1.0";
+    }
+
+    public iPhone(String modelo, String versaoSO) {
+        this.modelo = modelo;
+        this.versaoSO = versaoSO;
+    }
+
+    // Implementação de ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    // Implementação de AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Implementação de NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    // Getters e Setters
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getVersaoSO() {
+        return versaoSO;
+    }
+
+    public void setVersaoSO(String versaoSO) {
+        this.versaoSO = versaoSO;
+    }
+}
